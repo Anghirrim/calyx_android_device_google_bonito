0b62b8e3bb7459934bb08dd3e4653a3c2469d421
Anghirrim <anghirrim@gmail.com>
2025-05-06 21:19:09 +0200

    Adapt device tree for CalyxOS

--- Calyx_bonito.mk
+++ Calyx_bonito.mk
@@ -10,12 +10,12 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit device configuration
 $(call inherit-product, device/google/bonito/aosp_bonito.mk)
 
-include device/google/bonito/device-lineage.mk
+include device/google/bonito/device-Calyx.mk
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 3a XL
-PRODUCT_NAME := lineage_bonito
+PRODUCT_NAME := Calyx_bonito
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2160
--- Calyx_sargo.mk
+++ Calyx_sargo.mk
@@ -10,12 +10,12 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit device configuration
 $(call inherit-product, device/google/bonito/aosp_sargo.mk)
 
-include device/google/bonito/device-lineage.mk
+include device/google/bonito/device-Calyx.mk
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 3a
-PRODUCT_NAME := lineage_sargo
+PRODUCT_NAME := Calyx_sargo
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2220
